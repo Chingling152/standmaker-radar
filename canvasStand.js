85ddb757b14311d090b84f10936c0b9b3a3fd77f
Franklin Bitencourt <Chingling152@hotmail.com>
2019-01-21 21:04:30 -0200

    V. 2.0 * Migrated from svg to canvas * Changed size from 400px to 400x400px

---
+++ canvasStand.js
@@ -0,0 +1,109 @@
+function Position(x,y) {
+    this.x = x;
+    this.y = y;
+}
+//ranks
+var Ranks  = ["?","E","D","C","B","A"];
+
+//texts
+var PowerText = new Position(192,75);
+var SpeedText = new Position(300,136);
+var RangeText = new Position(300,280);
+var DurabilityText = new Position(192,343);
+var PrecisionText = new Position(75,280);
+var PotentialText = new Position(75,136);
+
+//skills
+var PowerPos = [
+    new Position(199,200),//?
+    new Position(199,183),//E
+    new Position(199,165),//D
+    new Position(199,146),//C
+    new Position(199,128),//B
+    new Position(199,109)//A
+];
+var SpeedPos = [
+    new Position(199,200),//?
+    new Position(215,193),//E
+    new Position(230,184),//D
+    new Position(246,175),//C
+    new Position(262,165),//B
+    new Position(278,156)//A
+];
+var RangePos = [
+    new Position(199,200),//?
+    new Position(215,211),//E
+    new Position(231,221),//D
+    new Position(247,230),//C
+    new Position(263,239),//B
+    new Position(278,249)//A
+];
+
+var DurabilityPos =[
+    new Position(199,202),//?
+    new Position(199,221),//E
+    new Position(199,238),//D
+    new Position(199,257),//C
+    new Position(199,276),//B
+    new Position(199,294)//A
+];
+
+var PrecisionPos =[
+    new Position(199,200),//NONE
+    new Position(182,211),//E
+    new Position(168,220),//D
+    new Position(150,230),//C
+    new Position(134,239),//B
+    new Position(121,247)//A
+];
+
+var PotentialPos =[
+    new Position(199,200),//?
+    new Position(182,193),//E
+    new Position(168,184),//D
+    new Position(150,175),//C
+    new Position(134,165),//B
+    new Position(121,156)//A
+];
+
+// Objects
+var c = document.getElementById("canvasStand");
+var ctx = c.getContext("2d");
+
+var RankText = new Array(6);
+
+
+
+function SetValues(){
+    var Pow = document.getElementById("power").value;
+    var Spe = document.getElementById("speed").value;
+    var Ran = document.getElementById("range").value;
+    var Dur = document.getElementById("durability").value;
+    var Pre = document.getElementById("precision").value;
+    var Pot = document.getElementById("potential").value;
+
+    ctx.clearRect(0, 0, c.width, c.height);
+    DrawBackground();
+    DrawStats(Pow,Spe,Ran,Dur,Pre,Pot);
+
+}
+
+function DrawStats(Pow,Spe,Ran,Dur,Pre,Pot){
+    ctx.fillStyle = "rgb(250,0,0,0.5)";
+    ctx.beginPath();
+    ctx.moveTo(PowerPos[Pow].x,PowerPos[Pow].y);
+    ctx.lineTo(SpeedPos[Spe].x,SpeedPos[Spe].y);
+    ctx.lineTo(RangePos[Ran].x,RangePos[Ran].y);
+    ctx.lineTo(DurabilityPos[Dur].x,DurabilityPos[Dur].y);
+    ctx.lineTo(PrecisionPos[Pre].x,PrecisionPos[Pre].y);
+    ctx.lineTo(PotentialPos[Pot].x,PotentialPos[Pot].y);
+    ctx.closePath();
+    ctx.fill();
+}
+
+function DrawBackground(){
+    var img = document.getElementById("img");
+    ctx.drawImage(img,0,0,c.height,c.width);
+}
+
+SetValues();
